f996f67a1c9704f11c06040de4c70ed9b3d195fb
quanphan2906 <52489089+quanphan2906@users.noreply.github.com>
2020-06-17 16:25:51 +0700

    Delete config.js

--- server/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    PORT: 5000,
-    MONGO_ATLAS_URL:
-        "mongodb+srv://quanphan2906:anotherclouddatabase@blindblogging-0ybka.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "greatserver!",
-};
