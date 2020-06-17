dc1e3379e799f0fe90d6a0a1dda69fb0396272da
Quan Phan <hoangquanphan2906@gmail.com>
2020-06-17 15:48:43 +0700

    Adding config.js file

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: 5000,
+    MONGO_ATLAS_URL:
+        "mongodb+srv://quanphan2906:anotherclouddatabase@blindblogging-0ybka.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET: "greatserver!",
+};
